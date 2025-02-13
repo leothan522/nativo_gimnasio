55e19f722efa4b336f7f75163892a48bc17b0e5a
AntonnyMalu <gabrielmalu15@gmail.com>
2025-02-13 12:16:50 -0400

    modificaciones de interfaz visual

--- resources/views/layouts/finanza/footer.php
+++ resources/views/layouts/finanza/footer.php
@@ -1,10 +1,10 @@
 <div class="container py-5">
-    <div class="row g-5">
-        <div class="col-lg-3 col-md-6">
-            <h4 class="text-white mb-4">Our Office</h4>
-            <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>123 Street, New York, USA</p>
-            <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+012 345 67890</p>
-            <p class="mb-2"><i class="fa fa-envelope me-3"></i>info@example.com</p>
+    <div class="row g-5 justify-content-center">
+        <div class="col-lg-4 col-md-6">
+            <h4 class="text-white mb-4">Nuestra Sede</h4>
+            <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>2301 San Juan de los Morros, Venezuela</p>
+            <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>0246-431.26.40</p>
+            <p class="mb-2"><i class="fa fa-envelope me-3"></i>ccgymsanjuan@gmail.com</p>
             <div class="d-flex pt-2">
                 <a class="btn btn-square btn-outline-light rounded-circle me-2" href=""><i
                         class="fab fa-twitter"></i></a>
@@ -16,7 +16,7 @@
                         class="fab fa-linkedin-in"></i></a>
             </div>
         </div>
-        <div class="col-lg-3 col-md-6">
+        <div class="col-lg-3 col-md-6 d-none">
             <h4 class="text-white mb-4">Services</h4>
             <a class="btn btn-link" href="">Financial Planning</a>
             <a class="btn btn-link" href="">Cash Investment</a>
@@ -25,22 +25,13 @@
             <a class="btn btn-link" href="">Business Analysis</a>
         </div>
         <div class="col-lg-3 col-md-6">
-            <h4 class="text-white mb-4">Quick Links</h4>
-            <a class="btn btn-link" href="">About Us</a>
-            <a class="btn btn-link" href="">Contact Us</a>
-            <a class="btn btn-link" href="">Our Services</a>
-            <a class="btn btn-link" href="">Terms & Condition</a>
-            <a class="btn btn-link" href="">Support</a>
+            <h4 class="text-white mb-4">Enlaces rápidos</h4>
+            <a class="btn btn-link" href="">Membresía</a>
+            <a class="btn btn-link" href="">Contáctanos</a>
+            <a class="btn btn-link" href="">Perfil</a>
         </div>
-        <div class="col-lg-3 col-md-6">
-            <h4 class="text-white mb-4">Newsletter</h4>
-            <p>Dolor amet sit justo amet elitr clita ipsum elitr est.</p>
-            <div class="position-relative w-100">
-                <input class="form-control bg-white border-0 w-100 py-3 ps-4 pe-5" type="text"
-                       placeholder="Your email">
-                <button type="button"
-                        class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2">SignUp</button>
-            </div>
+        <div class="col-lg-3 col-md-6 ">
+            <img class="img-thumbnail" src="<?php asset('img/logo_gym.png'); ?>">
         </div>
     </div>
 </div>
--- resources/views/layouts/finanza/navbar.php
+++ resources/views/layouts/finanza/navbar.php
@@ -8,10 +8,10 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarCollapse">
         <div class="navbar-nav ms-auto p-4 p-lg-0">
-            <a href="index.html" class="nav-item nav-link active">Home</a>
-            <a href="about.html" class="nav-item nav-link">About</a>
-            <a href="service.html" class="nav-item nav-link">Services</a>
-            <a href="contact.html" class="nav-item nav-link">Contact</a>
+            <a href="index.html" class="nav-item nav-link active">Inicio</a>
+            <a href="about.html" class="nav-item nav-link">Membresía</a>
+            <!--<a href="service.html" class="nav-item nav-link">Services</a>-->
+            <a href="contact.html" class="nav-item nav-link">Contáctanos</a>
             <!--<div class="nav-item dropdown">
                 <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
                 <div class="dropdown-menu border-light m-0">
